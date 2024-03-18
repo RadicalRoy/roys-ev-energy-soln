8b1297a3adf0d14df5eb10f914c7b5f0f07ab357
radicalroy <roy@thexcompany.com>
2024-03-18 15:59:55 -0700

    feat: request location permission and fetch stations near users location

--- app/(tabs)/search/index.tsx
+++ app/(tabs)/search/index.tsx
@@ -2,16 +2,19 @@ import { StyleSheet } from "react-native";
 import * as Location from "expo-location";
 import { useEffect, useState } from "react";
 
-import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
+import { Station, fetchNearbyStations } from "./api";
 
-export default function TabTwoScreen() {
+export default function SearchScreen() {
   const [location, setLocation] = useState<Location.LocationObject | null>(
     null
   );
+  const [stations, setStations] = useState<Station[]>([]);
+  const [selectedStation, setSelectedStation] = useState<number>();
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
 
   useEffect(() => {
+    // request location permission
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") {
@@ -25,6 +28,19 @@ export default function TabTwoScreen() {
     })();
   }, []);
 
+  useEffect(() => {
+    if (!location) return;
+    // when location changes, get list of nearby charging stations
+    const { latitude, longitude } = location?.coords;
+    // I'm aware of hanging promise issues with this approach (e.g. if user navigates away while waiting for response)
+    (async () => {
+      const newStations = await fetchNearbyStations(latitude, longitude);
+      setStations(newStations);
+    })();
+  }, [location]);
+
+  console.log("stations", stations);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
@@ -33,7 +49,6 @@ export default function TabTwoScreen() {
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
-      {/* <EditScreenInfo path="app/(tabs)/search.tsx" /> */}
     </View>
   );
 }
